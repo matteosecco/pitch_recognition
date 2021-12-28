1a60ae4275084be1e1de2b0e6ecef70f300f6560
matteosecco <36543260+matteosecco@users.noreply.github.com>
2021-12-28 14:04:54 +0100

    Manca getnote

--- audioedit.py
+++ audioedit.py
@@ -1,14 +1,21 @@
 # Library contaning functions that edit the sound data
 
 
+from matplotlib import pyplot as plt
 import math
-from functions import *
+from functions import smooth, getsign, sinfind, der, mean
+from reader import wavreader, wavwriter
+from copy import copy
 
 
 def freqcalc(l):
     """ Calculates the frequence of a list of samples
         frequency is defined as the distance (number of samples) between
-        a zero and another """
+        a zero and another
+        
+        l: list of wave frames
+        returns: list containing a value of frequency (actually wavelenght)
+        for each value """
 
     # where to save the frequencies
     freq_list = []
@@ -31,57 +38,91 @@ def freqcalc(l):
     return freq_list
 
 
-def freqcalc2(l):
-    """ Calculates the frequence of a list of samples
-        frequency is defined as the distance between two hights with between
-        them at least a negative value """
+def maxlist(l):
+    """ Returns the list of all the local maximum and minimum
+        a maximum is defined as the local maximum in an interval where the
+        extremesare equal to 0
+        
+        l: list of wave frames 
+        returns: list of tuples (idx, value) """
 
-    freq_list = []
+    sign = getsign(l[0])
+    locbest = 0
+    idxbest = 0
 
-    sign = 1
-    oldbestidx = 0
-    best, bestidx = 0, 0
+    # list of 2d tuples
+    data = []
 
     for i in range(len(l)):
+        if abs(l[i]) > locbest:
+            locbest = abs(l[i])
+            idxbest = i
 
-        if getsign(l[i]) != sign:
-            if sign == 1:
-
-                # append the last frequency
-                freq_list.extend([bestidx-oldbestidx+1]
-                                 * (bestidx-oldbestidx+1))
-
-                oldbestidx = bestidx
-                best, bestidx = 0, 0
+        # the value must be different from 0 to avoid a very rare case where
+        # the first derivative is 0 exactly on y=0 so that getsign(0) returns -1
+        if getsign(l[i]) != sign and l[i] != 0:
+            data.append((idxbest, locbest*sign))
             sign = getsign(l[i])
+            locbest = 0
 
-        if l[i] > best:
-            best = l[i]
-            bestidx = i
+    # adds first and last point to coincide with 0
+    if data[0][0] != 0:
+        data.insert(0, (0, 0))
+    data.append((len(l), 0))
 
-    freq_list.extend([bestidx-oldbestidx] * (bestidx-oldbestidx))
-    return freq_list
-
-
-data = wavreader("tredowav.wav")
-
-ch1 = data["data"]["ch1"][:]
-ch2 = data["data"]["ch2"]
-freqs = freqcalc(ch1)
-
-fig, ax = plt.subplots()
-
-ax2 = ax.twinx()
+    return data
 
 
-ax.grid()
-ax.plot(ch1)
-ax2.plot(freqs, color="red", linewidth=0.3)
+def createsin(l):
+    """ Creates a continous function from the list of points 'l'
+    
+    l: list of wave frames
+    returns: list with all the values indexed """
 
-ax.set_ylabel("int")
-ax2.set_ylabel("Hertz")
-ax.axhline(0, color="black", alpha=0.5)
-ax2.axhline(0, color="black", alpha=0.5)
+    new_l = []
+    # get the list of maximums and minimums
+    plist = maxlist(l)
+
+    # cycles all the points
+    for i in range(len(plist)-1):
+
+        # for each two point it calculates the right sine function
+        a, b, c, d = sinfind(plist[i], plist[i+1])
+
+        # for each two point generates all the point in between them
+        for x in range(plist[i][0], plist[i+1][0]):
+
+            value = a*math.sin(b*x + c) + d
+
+            new_l.append(int(value))
+
+    return new_l
 
 
-plt.savefig("plot.png", dpi=600)
+def getnote(l, r=44100):
+    """ Gets the mean frequency of the list
+    
+    l: list of wave frames
+    r: sample rate per second
+    returns: int representing Hertz """
+
+    d = {"do4": 440,
+         "do#4": 466,
+         "re4": 493,
+         "mi5": 523,
+         "mi#5": 554,
+         "fa5": 587,
+         "fa#5": 622,
+         "sol5": 659,
+         "la5": 698,
+         "la#5": 739,
+         "si5": 783,
+         "si#5": 830,
+         "do5": 880}
+
+    hertz = r / mean(freqcalc(l))
+
+    for v in d.values():
+        pass
+
+    return
